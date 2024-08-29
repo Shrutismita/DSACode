19b66e0431edc95e8c949cd8a178994a2d517b9c
Shrutismita <k.s.sruti@gmail.com>
2024-08-29 23:16:56 +0530

    Create delete-the-middle-node-of-a-linked-list.cs

---
+++ LinkedList/delete-the-middle-node-of-a-linked-list.cs
@@ -0,0 +1,22 @@
+Q:- https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
+
+Ans:
+public class Solution {
+    public ListNode DeleteMiddle(ListNode head) {
+        if(head == null || head.next == null)
+        {
+            return null;
+        }
+        ListNode prev = null;
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null)
+        {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = slow.next;
+        
+        return head;
+    }
+}
