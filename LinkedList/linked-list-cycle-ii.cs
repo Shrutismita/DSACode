797f574de64f0699b78596b44a9095a0e279a5f5
Shrutismita <k.s.sruti@gmail.com>
2024-08-29 22:42:43 +0530

    Create linked-list-cycle-ii.cs

---
+++ LinkedList/linked-list-cycle-ii.cs
@@ -0,0 +1,27 @@
+Q: https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+Ans:
+
+public class Solution {
+    public ListNode DetectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode slow = head, fast=head;
+        ListNode ptr = head;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+             break;
+         
+        }
+        if(slow != fast) return null;
+
+         while(ptr != slow)
+         {
+            ptr = ptr.next;
+            slow = slow.next;            
+         }
+          return ptr;
+    }
+}
