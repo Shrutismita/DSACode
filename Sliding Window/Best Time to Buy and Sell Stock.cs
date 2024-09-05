f4afd044149c04cf99a77c9a970b1c1dd5344490
Shrutismita <k.s.sruti@gmail.com>
2024-09-05 19:33:02 +0530

    Update Best Time to Buy and Sell Stock.cs

--- Sliding Window/Best Time to Buy and Sell Stock.cs
+++ Sliding Window/Best Time to Buy and Sell Stock.cs
@@ -1,4 +1,5 @@
 Q:- 121. https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+TC :- O(n)
 ================================================================================C#=========================
   public class Solution {
     public int MaxProfit(int[] prices) {
