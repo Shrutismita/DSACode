9be41394a30263665dfc381c7ab1d2e5e39756a4
Shrutismita <k.s.sruti@gmail.com>
2024-09-23 22:52:43 +0530

    Update Number of Provinces(DFS).java

--- Graph/Number of Provinces(DFS).java
+++ Graph/Number of Provinces(DFS).java
@@ -48,42 +48,27 @@ S.C:- O(V)
   //Approach-2 : [ Using DFS - Using given input graph]
 
   class Solution {
-     
-    void dfs(Map<Integer, List<Integer>> adj , int u ,boolean[] visited )
-    {
-        visited[u] = true;
+     private void dfs(int adj[][],int u ,boolean [] visited){
+        visited[u]=true;
         //Visit neighbours
-        //Find neighbours
-        for(int v : adj.get(u)) {
-           if(!visited[v]) {
-                dfs(adj, v, visited);
+        for (int v=0;v<adj.length;v++){
+            if(adj[u][v]==1 && visited[v]==false){
+                dfs(adj,v,visited);
             }
-        }
+
+       }
     }
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-       
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            adj.put(i, new ArrayList<>());
-        }
-         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n ; j++) {
-                if (isConnected[i][j] == 1)
-                    adj.get(i).add(j);
-            }
-        }
-         boolean[] visited = new boolean[n];
-         int count = 0;
-        for(int i = 0; i < n; i++)
-        {
-            if(visited[i]== false)
-            {
-                 dfs(adj,i,visited);
-                 count++;
+        boolean[] visited =new boolean[isConnected.length];
+        int count=0;
+        for (int i=0;i<isConnected.length;i++){
+            if(!visited[i]){
+                dfs(isConnected,i,visited);
+                count++;
             }
         }
         return count;
+        
     }
+   
 }
-  
