9ca008697446ed33d18d2f37a5a9a0c4d7d54b90
Shrutismita <k.s.sruti@gmail.com>
2024-09-24 17:17:28 +0530

    Update Course Schedule II.java

--- Graph/Course Schedule II.java
+++ Graph/Course Schedule II.java
@@ -61,3 +61,60 @@ S.C:- O(V)
     }
 }
 ======================================================================================================================================
+//Approach-2 (Using DFS)
+  
+  class Solution {
+      boolean hasCycle = false;
+      void isCycleDfs(List<List<Integer>> adj,int u,boolean[] visited,Stack<Integer> st,boolean[] inRecursion)
+      {
+        visited[u] = true;
+        inRecursion[u] = true;
+         //first we put u node's child in to a stack
+        for(int v : adj.get(u))
+        {
+            if(inRecursion[v] == true)
+            {
+                hasCycle = true;
+                return;
+            }
+            if(visited[v] == false)
+                isCycleDfs(adj,v,visited,st,inRecursion);
+        }
+            //now put u node in to a stack
+          st.push(u);
+          inRecursion[u] = false;
+      }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        boolean[] visited = new boolean[numCourses];
+        boolean[] inRecursion = new boolean[numCourses];
+        Stack<Integer> st = new Stack();
+        for(int i = 0; i < numCourses; i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] prerequisite : prerequisites)
+        {
+            int a = prerequisite[0];
+            int b = prerequisite[1];
+
+            adj.get(b).add(a);
+        }
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(visited[i] == false)            
+                  isCycleDfs(adj,i,visited,st,inRecursion);            
+        }
+         int[] result = new int[numCourses];
+        if(hasCycle)
+        {
+            return new int[0];
+        }
+        int k = 0;
+        while(!st.isEmpty())
+        {
+            result[k++] = st.pop();
+        }
+        return result;
+    }
+}
