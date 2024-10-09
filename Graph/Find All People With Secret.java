4ac6e87343c4934aff6b8eb130dfa25659cfaec8
Shrutismita <k.s.sruti@gmail.com>
2024-10-09 22:50:02 +0530

    Update Find All People With Secret.java

--- Graph/Find All People With Secret.java
+++ Graph/Find All People With Secret.java
@@ -107,3 +107,64 @@ Q:- https://leetcode.com/problems/find-all-people-with-secret/
             }
        }
 }
+************************************************************************************************
+//Approach-3 (Using min-heap(PriorityQueue) BFS to fetch earliest time)
+//T.C : ~O((N+M) * (log(M+N) + M)) 
+//S.C : O(N+M)
+---------------------------------------------------------------------------------
+  class Solution {
+   
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i =0; i < n; i++)
+            adj.add(new ArrayList<>());
+
+        for(int[] meeting : meetings)
+        {
+            int person1 = meeting[0];
+            int person2 = meeting[1];
+            int time = meeting[2];
+
+            adj.get(person1).add(new int[]{person2,time});
+            adj.get(person2).add(new int[]{person1,time});
+        }
+        // Priority Queue for BFS. It will store (time of knowing the secret, person)
+        // We will pop the person with the minimum time of knowing the secret.
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.add(new int[]{0, 0});
+        pq.add(new int[]{0, firstPerson});
+
+        // Visited array to mark if a person is visited or not.
+        // We will mark a person as visited after it is dequeued
+        // from the queue.
+        boolean[] visited = new boolean[n];
+
+        // Do BFS, but pop minimum.
+        while (!pq.isEmpty()) {
+            int[] timePerson = pq.poll();
+            int time = timePerson[0], person = timePerson[1];
+            if (visited[person]) {
+                continue;
+            }
+            visited[person] = true;
+            for (int[] nextPersonTime : adj.get(person))
+            {
+                int nextPerson = nextPersonTime[0];
+                int t = nextPersonTime[1];                
+                if (!visited[nextPerson] && t >= time) {
+                    pq.add(new int[]{t, nextPerson});
+                }
+            }
+        }
+        
+        // Since we visited only those people who know the secret
+        // we need to return indices of all visited people.
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            if (visited[i]) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
