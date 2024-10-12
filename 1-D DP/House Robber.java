f09fc81a669ce7c1a5987c5473642723be4f4245
Shrutismita <k.s.sruti@gmail.com>
2024-10-12 18:14:37 +0530

    Create House Robber.java

---
+++ 1-D DP/House Robber.java
@@ -0,0 +1,48 @@
+Q:- https://leetcode.com/problems/house-robber/
+*********************************************************************************************
+//Approach-1 (Using Recursion + Memoization / Top Down)
+//T.C - O(2^n)
+//S.C- O(n)
+--------------------------------------------------
+  class Solution {
+    int[] dp;
+    int solve(int[] nums, int i , int n)
+    {
+        if(i >= n) return 0;
+        if(dp[i] != -1)
+             return dp[i];
+        int steal = nums[i] + solve(nums,i+2,n);
+        int skip = solve(nums, i+1,n);
+        return dp[i] = Math.max(steal,skip);
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        dp = new int[101];
+        Arrays.fill(dp, -1);
+        return solve(nums, 0,n);
+    }
+}
+========================================================================================
+//Approach-2 (Bottom up)
+//T.C - O(n)
+//S.C- O(n)
+--------------------------------------------------
+  class Solution {   
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        if(n == 1)
+            return nums[0];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i =2; i <= n; i++)
+        {
+            int steal = dp[i-1];
+            int skip = nums[i-1]+dp[i-2];
+
+            dp[i] = Math.max(steal,skip);
+        }
+        return dp[n];
+    }
+}
