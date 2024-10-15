6e885b226e0ae5120449f810ea666a83cc752138
Shrutismita <k.s.sruti@gmail.com>
2024-10-15 22:19:07 +0530

    Create Longest Palindromic Substring.java

---
+++ DP/Classical Dp problems/Longest Palindromic Substring.java
@@ -0,0 +1,48 @@
+Q:- https://leetcode.com/problems/longest-palindromic-substring/
+**************************************************************************
+//Approach 1 - Recursion + Memoization
+//T.C : O(n^2) - Because the AMortized Time Complexity of solve() will become 1 due to memoization.
+//S.C : O(n^2)
+----------------------------------------------------------------------
+  class Solution {
+    private int[][] t;
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int maxlen = Integer.MIN_VALUE;
+        int startingIndex = 0;
+        t = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(t[i], -1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(s, i, j) && j - i + 1 > maxlen) {
+                    startingIndex = i;
+                    maxlen = j - i + 1;
+                }
+            }
+        }
+
+        return s.substring(startingIndex, startingIndex + maxlen);
+    }
+
+    private boolean solve(String s, int l, int r) {
+        if (l >= r) {
+            return true;
+        }
+
+        if (t[l][r] != -1) {
+            return t[l][r] == 1;
+        }
+
+        if (s.charAt(l) == s.charAt(r)) {
+            t[l][r] = solve(s, l + 1, r - 1) ? 1 : 0;
+        } else {
+            t[l][r] = 0;
+        }
+
+        return t[l][r] == 1;
+    }
+}
