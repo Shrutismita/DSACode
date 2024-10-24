a5b94fe68e49f91bb892c0d8a5fbe43fe19c693a
Shrutismita <k.s.sruti@gmail.com>
2024-10-24 22:15:08 +0530

    Update Scramble String.java

--- DP/Scramble String.java
+++ DP/Scramble String.java
@@ -1,8 +1,8 @@
 Q:- https://leetcode.com/problems/scramble-string/
 *************************************************************
-  //Appoach- (Recursion + Memo)
-  ---------------------------------------
-  class Solution {
+//Appoach- (Recursion + Memo)
+--------------------------------------------------
+ class Solution {
       Map<String, Boolean> mp = new HashMap<>();
       boolean solve(String s1, String s2)
       {
@@ -12,17 +12,36 @@ Q:- https://leetcode.com/problems/scramble-string/
             String key = s1 + " " + s2;
 
         if (mp.containsKey(key)) return mp.get(key);
-
+        // boolean result = false;
         for (int i = 1; i < n; i++) {
-            if (solve(s1.substring(0, i), s2.substring(0, i)) && solve(s1.substring(i), s2.substring(i))){
-                mp.put(key, true);
-                return true;
-            }
+              /*
+            Example : "great"  "eatgr"
+            if swapped at i = 2
+            we compare s1.substr(0, i) i.e "gr"  with s2.substr(n-i, i) i.e. "gr"
+            && s1.substr(i, n-i) i.e "eat"  with s2.substr(0, n-i) i.e. "eat"
+            */
 
-            if (solve(s1.substring(0, i), s2.substring(n - i)) && solve(s1.substring(i), s2.substring(0, n - i))){
+            boolean swapped = solve(s1.substring(0, i), s2.substring(0, i)) 
+                              && solve(s1.substring(i), s2.substring(i));
+            if(swapped) { //if we find they are scrambled, we don't need to check further
                 mp.put(key, true);
                 return true;
             }
+            /*
+            Example : "great"  "great"
+            if not swapped at i = 2
+            we compare s1.substr(0, i) i.e "gr"  with s2.substr(0, i) i.e. "gr"
+            && s1.substr(i, n-i) i.e "eat"  with s2.substr(i, n-i) i.e. "eat"
+            */
+
+            boolean not_swapped = solve(s1.substring(0, i), s2.substring(n - i)) &&
+                               solve(s1.substring(i), s2.substring(0, n - i));
+            if(not_swapped) { //if we find they are scrambled, we don't need to check further
+                mp.put(key, true);
+                return true;
+               
+            }
+            
         }
 
         mp.put(key, false);
