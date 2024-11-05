91e15fe96940739a896ca3656ff1fba513815081
Shrutismita <k.s.sruti@gmail.com>
2024-11-05 17:46:16 +0530

    Create Last Stone Weight II.java

---
+++ DP/Last Stone Weight II.java
@@ -0,0 +1,24 @@
+Q:- https://leetcode.com/problems/last-stone-weight-ii/
+**************************************************************************
+ class Solution {
+      private int helper(int[] stones, int index, int sumL, int sumR, Integer[][] dp) {
+        if(index == stones.length){
+            return Math.abs(sumL - sumR);
+        }
+        
+        if(dp[index][sumL] != null) {
+            return dp[index][sumL];
+        }
+        
+        dp[index][sumL] = Math.min(helper(stones, index+1, sumL + stones[index], sumR, dp) , helper(stones, index+1, sumL, sumR + stones[index], dp));
+        return dp[index][sumL];
+    }
+    public int lastStoneWeightII(int[] stones) {
+        int sumStWt = 0;
+        for(int stone : stones){
+            sumStWt += stone;
+        }
+        Integer[][] dp = new Integer[stones.length][sumStWt];
+        return helper(stones, 0, 0, 0, dp);
+    }
+}
