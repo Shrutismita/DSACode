d3397edb658d25206c3db68a4d91bda10e375948
Shrutismita <k.s.sruti@gmail.com>
2024-11-12 23:32:32 +0530

    Create Sum of Beauty of All Substrings.java

---
+++ Strings/Sum of Beauty of All Substrings.java
@@ -0,0 +1,27 @@
+Q:- https://leetcode.com/problems/sum-of-beauty-of-all-substrings/
+******************************************************************************
+  class Solution {
+    public int beautySum(String s) {
+         int n = s.length();
+         int totalBeautySum = 0; 
+          for(int i = 0; i < n; i++)
+          {
+              HashMap<Character, Integer> frequencyMap = new HashMap<>();
+              for(int j = i; j < n; j++)
+              {
+                   char currChar = s.charAt(j);
+                   frequencyMap.put(currChar, frequencyMap.getOrDefault(currChar, 0) + 1);
+
+                   int minFreq = Integer.MAX_VALUE;
+                   int maxFreq = Integer.MIN_VALUE;
+                   for(int freq : frequencyMap.values())
+                   {
+                       minFreq = Math.min(minFreq, freq);
+                       maxFreq = Math.max(maxFreq, freq);
+                    }
+                totalBeautySum += (maxFreq - minFreq);
+              }
+          }
+           return totalBeautySum;
+    }
+}
