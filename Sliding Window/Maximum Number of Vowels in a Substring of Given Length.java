57b4a460b674a76329bd996b5caa0c57a2c1d4ec
Shrutismita <k.s.sruti@gmail.com>
2024-11-24 04:34:45 +0530

    Create Maximum Number of Vowels in a Substring of Given Length.java

---
+++ Sliding Window/Maximum Number of Vowels in a Substring of Given Length.java
@@ -0,0 +1,34 @@
+Q:- https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+ Company Tags                : MICROSOFT
+****************************************************************************************************
+  class Solution {
+    boolean isVowel(char ch)
+    {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    public int maxVowels(String s, int k) {
+        int n = s.length();
+        int maxV = 0;
+        int count = 0;
+        int i = 0, j = 0;
+
+        while(j < n)
+        {
+            if(isVowel(s.charAt(j)))
+            {
+                count++;
+            }
+            if(j-i+1 == k)
+            {
+                maxV = Math.max(maxV,count);
+                if(isVowel(s.charAt(i)))
+                {
+                    count--;                    
+                }
+                i++;
+            }
+            j++;
+        }
+        return maxV;
+    }
+}
