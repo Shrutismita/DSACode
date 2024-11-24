b0688403d6f2af7e075a07d35bb695546f731a56
Shrutismita <k.s.sruti@gmail.com>
2024-11-24 09:07:46 +0530

    Update Longest Subarray of 1's After Deleting One Element.java

--- Sliding Window/Longest Subarray of 1's After Deleting One Element.java
+++ Sliding Window/Longest Subarray of 1's After Deleting One Element.java
@@ -1,8 +1,38 @@
 Q:- 1493. https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
 Company Tags                : Google, Meta
 ======================================================================================================
+//Approach-1 (Using Traditional Sliding Window) 
+//T.C- O(n)    
+------------------------------------------------------
+    class Solution {
+    public int longestSubarray(int[] nums) {
+        int n = nums.length;
+         int zeroCount = 0;
+        int longestWindow = 0;
+
+        int i = 0;
+        for(int j = 0; j < n; j++)
+        {
+            if(nums[j] == 0)
+               zeroCount++;
+
+               while(zeroCount >1)
+               {
+                   if(nums[i] == 0)
+                   {
+                    zeroCount--;
+                   }
+                   i++;
+               }
+               longestWindow = Math.max(longestWindow,j-i);
+        }
+       return longestWindow;
+    }
+}
+=========================================================================================
+//Approach-2 (Using Better Sliding Window) 
 //T.C:- O(n)
-----------------------    
+-------------------------------------------------    
 class Solution {
     public int longestSubarray(int[] nums) {
         int n = nums.length;
