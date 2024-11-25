c7e646fa2ad50b5d126ec959258c6e733270b0e3
Shrutismita <k.s.sruti@gmail.com>
2024-11-25 20:48:23 +0530

    Create Broken Calculator.java

---
+++ Greedy/Broken Calculator.java
@@ -0,0 +1,17 @@
+Q:- https://leetcode.com/problems/broken-calculator/
+Company Tags                : Samsung
+******************************************************************************************
+  class Solution {
+    public int brokenCalc(int startValue, int target) {
+        
+        if(startValue >= target)
+                return startValue - target;
+
+          if(target%2 == 0)
+          {
+            return 1 + brokenCalc(startValue,target/2);
+          }      
+
+          return 1+brokenCalc(startValue,target+1);
+    }
+}
