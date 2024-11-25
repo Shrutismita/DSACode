e09f1e48116bc2bd3d0b2c038ccdc871bd424dc3
Shrutismita <k.s.sruti@gmail.com>
2024-11-25 20:46:03 +0530

    Create Break a Palindrome.java

---
+++ Greedy/Break a Palindrome.java
@@ -0,0 +1,20 @@
+Q:- https://leetcode.com/problems/break-a-palindrome/
+ Company Tags                : Amazon, Google(with some variation)
+***************************************************************************************************
+  class Solution {
+    public String breakPalindrome(String palindrome) {
+        int n = palindrome.length();
+        if(n == 1) return "";
+         char[] ch = palindrome.toCharArray();
+        for(int i = 0; i < n/2; i++)
+        {
+            if(ch[i] != 'a')
+            {
+                ch[i] = 'a';
+                return String.valueOf(ch);
+            }
+        }
+        ch[n-1] = 'b';
+        return String.valueOf(ch);
+    }
+}
