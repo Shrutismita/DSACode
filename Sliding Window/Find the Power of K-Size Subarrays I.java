7b508160bf0425ea4012e0cef8dade0bda6d6373
Shrutismita <k.s.sruti@gmail.com>
2024-12-07 23:19:42 +0530

    Create Find the Power of K-Size Subarrays I.java

---
+++ Sliding Window/Find the Power of K-Size Subarrays I.java
@@ -0,0 +1,50 @@
+Q:- https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/
+**************************************************************************************
+//Approach (Using sliding window)
+//T.C : O(n)
+//S.C : O(1)
+--------------------------------------------------
+  class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n-k+1];
+        Arrays.fill(result, -1);
+
+        int count = 1; // Count of consecutive elements
+
+        // Preprocess the first window
+        for (int i = 1; i < k; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+        }
+
+        // Check if the first window satisfies the condition
+        if (count == k) {
+            result[0] = nums[k - 1];
+        }
+
+        int i = 1;
+        int j = k;
+
+        // Process the sliding window
+        while (j < n) {
+            if (nums[j] == nums[j - 1] + 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count >= k) {
+                result[i] = nums[j];
+            }
+
+            i++;
+            j++;
+        }
+
+        return result;
+    }
+}
