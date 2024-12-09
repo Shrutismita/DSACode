a6681c29c27452f48e9e5e75b4a290bfb27bb9bf
Shrutismita <k.s.sruti@gmail.com>
2024-12-09 23:26:09 +0530

    Create Bulb Switcher.java

---
+++ Mathematical/Bulb Switcher.java
@@ -0,0 +1,8 @@
+Q:- https://leetcode.com/problems/bulb-switcher/description/
+ Company Tags                : AMAZON
+ ********************************************************************************
+  class Solution {
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+}
